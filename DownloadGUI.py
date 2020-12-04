c328ad9fdc69c350fe2cfa35fd5b681c50747818
BrawlyTheInventor <49785023+BrawlyTheInventor@users.noreply.github.com>
2020-12-04 10:17:12 -0500

    Update DownloadGUI.py
    It is a mess

--- DownloadGUI.py
+++ DownloadGUI.py
@@ -1,25 +1,22 @@
+import subproess
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+app = QApplication([])
+app.setStyle('Fusion')
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QLabel('Electric GUI'))
+layout.addWidget(QLineEdit()
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+window.show()
+app.exec_()
 
-from appJar import *
-import subprocess
-app = gui("Package downloader", "500x200")
-app.setBg("#bd93f9")
-app.setFont(18)
-
-def press(button):
-    if button == "Cancel":
-        app.stop()
-    if button == 'Uninstall':
+  #uninstall electric package
         pkg = app.getEntry("Package Name  ")
         proc = subprocess.Popen(f'electric uninstall {pkg}')
         output, err = proc.communicate()
     
-    else:
-      pkg = app.getEntry("Package Name  ")
+#Install electric package
+pkg = app.getEntry("Package Name  ")
       proc = subprocess.Popen(f'electric install {pkg}')
       output, err = proc.communicate()
-    
-app.addLabel("title", "Installer GUI")
-app.addLabelEntry("Package Name  ")
-app.addButtons(["Install", "Uninstall", "Cancel"], press)
-
-app.go()
